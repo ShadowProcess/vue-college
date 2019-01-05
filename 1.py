a0e519fcb22f7b87ed40252753b7f51887d9deee
alex <jiahangsocket@163.com>
2019-01-05 08:32:24 +0800

    图片资源

---
+++ 1.py
@@ -0,0 +1,19 @@
+import datetime
+import random
+import os
+
+def set_sys_date(year, month, day):
+    print('date %04d/%02d/%02d' % (year, month, day))
+    os.system('date %04d/%02d/%02d' % (year, month, day))
+
+def set_sys_time():
+    os.system('time %02d:%02d:%02d' % (random.randint(8,23), random.randint(0,60),random.randint(0,60)))
+
+def commit(msg):
+    os.system('git add .')
+    os.system('git commit -m %s' % (msg))
+
+if __name__ == '__main__':
+    set_sys_date(2019,1,5)   #设置那一天
+    set_sys_time()
+    commit("图片资源")           #想要深色点，最低三个提交
