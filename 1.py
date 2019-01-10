9e1e991b1d7b218da41a4eb648d76c4f08700496
alex <jiahangsocket@163.com>
2019-01-10 20:34:50 +0800

    引入依赖模块

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2019,1,9)          #设置那一天
+    set_sys_date(2019,1,10)          #设置那一天
     set_sys_time()
-    commit("es运算符3个点什么意思")           #想要深色点，最低三个提交
+    commit("引入依赖模块")           #想要深色点，最低三个提交
