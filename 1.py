03124ba4e84ae008ff4fecca5d93393e48085a81
alex <jiahangsocket@163.com>
2019-01-28 08:39:24 +0800

    v-for

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2019,1,26)        #设置那一天
+    set_sys_date(2019,1,28)        #设置那一天
     set_sys_time()
-    commit("数组")           #想要深色点，最低三个提交
+    commit("v-for")           #想要深色点，最低三个提交
