8bebfb57d14eeb6a713e122c94cfe21f49447c1b
alex <jiahangsocket@163.com>
2019-02-01 09:27:51 +0800

    vue-键盘事件

--- 1.py
+++ 1.py
@@ -16,4 +16,4 @@ def commit(msg):
 if __name__ == '__main__':
     set_sys_date(2019,2,1)        #设置那一天
     set_sys_time()
-    commit("vue--event")           #想要深色点，最低三个提交
+    commit("vue-键盘事件")           #想要深色点，最低三个提交
