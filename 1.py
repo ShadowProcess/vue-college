a5f59bdd3daf2cb0b2a4a76b994bcb29f1fa828a
alex <jiahangsocket@163.com>
2019-02-02 21:56:52 +0800

    bootstrap

--- 1.py
+++ 1.py
@@ -16,4 +16,4 @@ def commit(msg):
 if __name__ == '__main__':
     set_sys_date(2019,2,2)        #设置那一天
     set_sys_time()
-    commit("promise函数")           #想要深色点，最低三个提交
+    commit("bootstrap")           #想要深色点，最低三个提交
