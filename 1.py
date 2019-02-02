0e76393061b19ed3afbb86698df62f0ac87f227b
alex <jiahangsocket@163.com>
2019-02-02 13:16:45 +0800

    axios-异步请求框架

--- 1.py
+++ 1.py
@@ -7,7 +7,7 @@ def set_sys_date(year, month, day):
     os.system('date %04d/%02d/%02d' % (year, month, day))
 
 def set_sys_time():
-    os.system('time %02d:%02d:%02d' % (random.randint(8,23), random.randint(0,60),random.randint(0,60)))
+    os.system('time %02d:%02d:%02d' % (random.randint(8,23), random.randint(0,59),random.randint(0,59)))
 
 def commit(msg):
     os.system('git add .')
@@ -16,4 +16,4 @@ def commit(msg):
 if __name__ == '__main__':
     set_sys_date(2019,2,2)        #设置那一天
     set_sys_time()
-    commit("bootstrap")           #想要深色点，最低三个提交
+    commit("axios-异步请求框架")           #想要深色点，最低三个提交
