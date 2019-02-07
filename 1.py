5db32cf089fb400bd01abf500988abf0ae6a2d55
alex <jiahangsocket@163.com>
2019-02-07 20:13:41 +0800

    vu动画

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2019,2,5)        #设置那一天
+    set_sys_date(2019,2,7)     #设置那一天
     set_sys_time()
-    commit("promise与ajax")           #想要深色点，最低三个提交
+    commit("vu动画")            #想要深色点，最低三个提交
