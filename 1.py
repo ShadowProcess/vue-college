622a965787c64eb3cff8a1f6442630d14ed963de
alex <jiahangsocket@163.com>
2019-02-09 16:10:09 +0800

    computed计算属性

--- 1.py
+++ 1.py
@@ -14,6 +14,6 @@ def commit(msg):
     os.system('git commit -m %s' % (msg))
 
 if __name__ == '__main__':
-    set_sys_date(2019,2,8)     #设置那一天
+    set_sys_date(2019,2,9)     #设置那一天
     set_sys_time()
-    commit("子孙事件")            #想要深色点，最低三个提交
+    commit("computed计算属性")            #想要深色点，最低三个提交
